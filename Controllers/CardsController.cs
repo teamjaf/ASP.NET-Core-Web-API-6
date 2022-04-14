5ad35cb2e28aefc41772bad9f59627e94a868192
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-04-14 12:11:01 +0100

    Api calling all cards done

---
+++ Controllers/CardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Card_Api.Controllers;
+
+public class CardsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Card_Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Injecting DbContex
+builder.Services.AddDbContext<CardsDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
 
 var app = builder.Build();
 
